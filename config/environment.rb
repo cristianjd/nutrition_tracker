28080df2b346ee497990bbe750be912d3a3a86e3
cristianjd <cristian.j.derr@gmail.com>
2013-12-27 00:20:16 -0800

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+NutritionTracker2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NutritionTracker2::Application.config.secret_token = '56e016e0b814bcc8ace16aa491b989a3c8564e0783b337de7dc04ea307c1408c9b3bff13a60eee12521ec08d8da3de753038c20331d7c54fe264ff3b8937c539'
